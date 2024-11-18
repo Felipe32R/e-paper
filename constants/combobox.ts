dc3e45862a29e16d88c954bdac9a26cf12c54aa4
felipemetaway <felipe@metaway.com>
2024-11-18 17:36:31 -0300

    feat: add comboboxes constants

---
+++ constants/combobox.ts
@@ -0,0 +1,25 @@
+export const originList = [
+  {
+    value: "Digitalizado",
+    label: "Digitalizado",
+  },
+  {
+    value: "Eletrônico",
+    label: "Eletrônico",
+  },
+];
+
+export const typeList = [
+  {
+    value: "Nota fiscal de serviço",
+    label: "Nota fiscal de serviço",
+  },
+  {
+    value: "Boleto",
+    label: "Boleto",
+  },
+  {
+    value: "Contrato",
+    label: "Contrato",
+  },
+];
