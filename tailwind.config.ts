53206dd8132de341353cf8e99c0652b4a323d26c
felipemetaway <felipe@metaway.com>
2024-11-18 17:39:31 -0300

    build: fix config files

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from "tailwindcss";
 
 export default {
+  darkMode: ["class"],
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -11,8 +12,32 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        text: {
+          primary: "#3A424E",
+          secondary: "#6B7280",
+        },
+        green: {
+          primary: {
+            main: "#05C151",
+            light: "#CAFFD6",
+            lighter: "#EEFFF2",
+            dark: "#079942",
+          },
+        },
+        backgroundBlur: {
+          neutral: "#191E29",
+        },
+        border: {
+          neutral: "#E5E7EB",
+          light: "#F9FAFB",
+        },
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
       },
     },
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 } satisfies Config;
