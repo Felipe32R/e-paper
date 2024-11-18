44da22cca48e172f914bfb56076e368d1f4ad846
felipemetaway <felipe@metaway.com>
2024-11-18 20:37:19 -0300

    feat: add responsivity

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default async function Home() {
   const data = await client.getAll();
 
   return (
-    <main className="w-full h-full flex flex-col gap-6 px-12 py-6">
+    <main className="w-full h-full flex flex-col gap-6  px-6 py-3  sm:px-12 sm:py-6">
       {/* @ts-ignore */}
       <DocsTable data={data} />
     </main>
